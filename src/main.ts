885594e989c85a99a88e2f36eda8ef5658420766
naffiq <abdu.galymzhan@gmail.com>
2018-05-03 21:25:52 +0600

    feat(server): set up expressjs server and babel

--- src/main.ts
+++ src/main.ts
@@ -1,3 +1,13 @@
-const a: string = 'Hello, world!'
+import express from 'express'
+import {createServer} from 'http'
 
-console.log(a)
+const app = express()
+
+app.get('/', (_, res) => res.send('<h1>How are you doing there?</h1>'))
+
+const SERVER_PORT = process.env.SERVER_PORT || 8080;
+const server = createServer(app)
+
+server.listen(SERVER_PORT, () => {
+    console.log(`Server is up and running on ${SERVER_PORT}`)
+})
