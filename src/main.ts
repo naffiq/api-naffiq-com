85aa3b9f13b38437f5eadd5940ff25346a5e3198
naffiq <naffiq@gmail.com>
2018-07-04 15:37:37 +0300

    feat(lint): updated codestyle

--- src/main.ts
+++ src/main.ts
@@ -1,19 +1,23 @@
-import express from 'express'
-import bodyParser from 'body-parser'
-import {graphqlExpress, graphiqlExpress} from 'apollo-server-express'
-import {schema} from './gql'
-import {createServer} from 'http'
-
-const app = express()
-
-app.get('/', (_, res) => res.send('<h1>What are you doing there?</h1>'))
-
-app.use('/graphql', bodyParser.json(), graphqlExpress({schema}))
-app.use('/graphiql', graphiqlExpress({endpointURL: '/graphql'}))
+import { graphiqlExpress, graphqlExpress } from "apollo-server-express";
+import bodyParser from "body-parser";
+import express from "express";
+import { createServer } from "http";
+import mongoose from "mongoose";
+import { schema } from "./gql";
 
 const SERVER_PORT = process.env.SERVER_PORT || 8080;
-const server = createServer(app)
+const MONGO_SERVER = process.env.MONGO_SERVER || "localhost:27017";
+
+mongoose.connect(`mongodb://${MONGO_SERVER}/naffiq-com`);
+const app = express();
+
+app.get("/", (_, res) => res.send("<h1>What are you doing there?</h1>"));
+
+app.use("/graphql", bodyParser.json(), graphqlExpress({ schema }));
+app.use("/graphiql", graphiqlExpress({ endpointURL: "/graphql" }));
+
+const server = createServer(app);
 
 server.listen(SERVER_PORT, () => {
-    console.log(`Server is up and running on ${SERVER_PORT}`)
-})
+  console.log(`Server is up and running on ${SERVER_PORT}`);
+});
