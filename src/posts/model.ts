cdacc26fa3ed349165656afdfd73ed8071a75ff2
naffiq <naffiq@gmail.com>
2018-07-04 15:35:10 +0300

    feat(mongoose): updated schema and typed model

--- src/posts/model.ts
+++ src/posts/model.ts
@@ -1,9 +1,41 @@
-export interface IPost {
-  id          : number,
-  title       : string,
-  slug        : string,
-  description : string,
-  body        : string,
-  image       : string,
-  isActive    : boolean
+import mongoose from "mongoose";
+
+export interface IPost extends mongoose.Document {
+  title: string;
+  slug: string;
+  description: string;
+  body: string;
+  image: string;
+  isActive: boolean;
 }
+
+const PostSchema = new mongoose.Schema({
+  title: {
+    default: "",
+    type: String,
+    required: "Provide a title at least, bro, common"
+  },
+  slug: {
+    type: String,
+    required:
+      "This should be generated out of title, are you hacking your way through?"
+  },
+  description: {
+    type: String,
+    required:
+      "You want it to be shared, right? Imagine the meta tags being blank in that case"
+  },
+  body: {
+    type: String,
+    required: "У тебя в сумке пусто, и сама ты пустая. 0/10"
+  },
+  image: String,
+  isActive: {
+    type: Boolean,
+    default: false
+  }
+});
+
+const Post = mongoose.model<IPost>("Post", PostSchema);
+
+export default Post;
